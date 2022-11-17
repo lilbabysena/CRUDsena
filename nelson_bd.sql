9c83f4f33c87b7d3d17efa25bdb210eb46a81af8
lilbabysena <118447491+lilbabysena@users.noreply.github.com>
2022-11-17 08:21:53 -0500

    Add files via upload

---
+++ nelson_bd.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-11-2022 a las 17:29:00
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `nelson_bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `aprendices`
+--
+
+CREATE TABLE `aprendices` (
+  `apre_id` int(10) NOT NULL,
+  `apre_tipoid` char(3) NOT NULL,
+  `apre_numid` char(20) NOT NULL,
+  `apre_nombres` char(60) NOT NULL,
+  `apre_apellidos` char(60) NOT NULL,
+  `apre_direccion` char(80) NOT NULL,
+  `apre_telefono` char(20) NOT NULL,
+  `apre_ficha` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fichas`
+--
+
+CREATE TABLE `fichas` (
+  `ficha_numero` int(10) DEFAULT NULL,
+  `ficha_programa` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `programa`
+--
+
+CREATE TABLE `programa` (
+  `programa_id` int(10) DEFAULT NULL,
+  `programa_nombre` char(20) NOT NULL,
+  `programa_tipo` char(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiposprograma`
+--
+
+CREATE TABLE `tiposprograma` (
+  `tiposp_id` int(10) DEFAULT NULL,
+  `tiposp_tipo` char(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usua_id` int(10) DEFAULT NULL,
+  `usua_nomuser` char(50) NOT NULL,
+  `usua_contra` char(20) NOT NULL,
+  `usua_tipo` char(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `aprendices`
+--
+ALTER TABLE `aprendices`
+  ADD PRIMARY KEY (`apre_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `aprendices`
+--
+ALTER TABLE `aprendices`
+  MODIFY `apre_id` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
